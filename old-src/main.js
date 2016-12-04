8e8bd40a63ef02981ec92e3bad836c3c159578ba
Alfred Kirakosian <alfredkirakosian@Alfreds-MacBook-Pro.local>
2016-12-03 18:37:52 -0800

    Remove old src

--- old-src/main.js
+++
@@ -1,9 +0,0 @@
-const game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'beerRun-game');
-
-game.state.add('boot', require('./states/boot'));
-game.state.add('game', require('./states/game'));
-game.state.add('menu', require('./states/menu'));
-game.state.add('preloader', require('./states/preloader'));
-game.state.add('gameover', require('./states/gameover'));
-
-game.state.start('boot');
