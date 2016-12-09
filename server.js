e21557fe2a86f212d52b956581f9d2d30acb5d13
mariano89 <marianoe1989@gmail.com>
2016-12-09 15:34:55 -0800

    remove build, add assets

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 3000;
 var app = express();
 // var path = require("path");
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req,res) {
